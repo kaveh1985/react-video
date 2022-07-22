2ac3effc5fd0a8ecf64155cd1ff1373e2588b7af
Ramzanzad Malek <kaveh.ramzanzad@gmail.com>
2022-07-22 15:59:56 +0200

    Update youtube.js

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY =  'AIzaSyDxEBYmBp-pnoOJjRYPzNhnXkcpBbzw08o';
+const KEY =  "apikey"
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
